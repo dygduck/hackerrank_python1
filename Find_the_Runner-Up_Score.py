786af18b43dcacc6753b882841b54c102ed7a2c0
Duygu <duyguebcim@yahoo.com>
2018-05-24 19:51:48 +0000

    hackerrank challenges solved

---
+++ Find_the_Runner-Up_Score.py
@@ -0,0 +1,39 @@
+"""
+Given the participants' score sheet for your University Sports Day,
+you are required to find the runner-up score. You are given scores.
+Store them in a list and find the score of the runner-up.
+
+Input Format
+
+The first line contains . The second line contains an array of
+integers each separated by a space.
+
+Constraints
+
+Output Format
+
+Print the runner-up score.
+
+Sample Input 0
+
+5
+2 3 6 6 5
+Sample Output 0
+
+5
+Explanation 0
+
+Given list is . The maximum score is , second maximum is .
+Hence, we print  as the runner-up score.
+"""
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    arr = map(int, raw_input().split())
+    arr.sort(reverse=True)
+    i = 0
+    while i < len(arr):
+        if arr[i+1] < arr[i]:
+            print arr[i+1]
+            break
+        i = i+1
